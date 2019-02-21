5aeb0127afa80c2a3dc927f6a281b98710b50b03
Piyush Mehta <me@piyushmehta.com>
2019-02-21 23:04:11 +0530

    Callback removed
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- server.js
+++ server.js
@@ -66,4 +66,4 @@ app.use(express.static(__dirname + '/views'));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port);
