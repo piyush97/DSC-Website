1fd531f56cd6393f3653cc2642a4cfd5b0c08523
Ishank <ishankdev@gmail.com>
2019-02-22 00:28:26 +0530

    update css

--- views/typer.js
+++ views/typer.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [ "Student Internships", "Projects", "Hackathons", "Makeathons","Contests","Workshops","SCAM!!"];
+  var dataText = [ "Student Internships.", "Projects.", "Hackathons.","Contests.","Workshops.","Get Started!!"];
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
